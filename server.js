d4fae9e935314473c8475f9624680e5e92e148b1
djbalabis <110417718+djbalabis@users.noreply.github.com>
2023-01-26 19:21:47 -0500

    Heroku deploy

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+app.listen(PORT, () => {
+  console.log(`express server running on port ${PORT}`);
+});
