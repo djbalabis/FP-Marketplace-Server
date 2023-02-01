1e537bd058a648f54b0d66ea2752d2d0c7305adf
Cory Bee <cbinder705@gmail.com>
2023-01-31 21:35:37 -0500

    added Profile.js, profileSeeds.json, seeds.js

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,7 +1,16 @@
 const db = require("../config/connection");
 const { Inventory } = require("../models");
-
+const profileSeeds = require("./profileSeeds");
 const inventoryData = require("./inventoryData.json");
+
+db.once("open", async () => {
+  try {
+    await Profile.create(profileSeeds);
+  } catch (err) {
+    throw err;
+  }
+});
+
 db.once("open", async () => {
   await Inventory.deleteMany({});
   const actualInventory = await Inventory.insertMany(inventoryData);
