172dd1b1ce680d4b57412abb3d23e8171b64ecac
Tim Mardis <mardistj96@yahoo.com>
2023-02-02 20:27:23 -0600

    Adding delete route

--- routes/api/inventoryRoutes.js
+++ routes/api/inventoryRoutes.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const {
   getInventory,
   createInventory,
+  deleteInventory,
 
 } = require('../../controllers/inventoryController');
 
 router.route('/').get(getInventory).post(createInventory);
+
+router.route('/:inventoryId').delete(deleteInventory);
 
 module.exports = router;
